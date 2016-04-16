c3aa0b2fd05954cbe0aff816493d20a795381d21
David Benoit <ddavidrb@gmail.com>
2016-04-15 22:06:41 -0400

    updates

--- music-generator.rkt
+++ music-generator.rkt
@@ -3,9 +3,10 @@
 (require rsound/single-cycle)
 (require "rsound-music-theory.rkt")
 
-
-
-(define (gen-beat-structure length)
+;; Take the length of the desired beat structure
+;; to be created in frames.  Subdivide it randomly 
+;; into powers of two.
+(define (gen-random-beat-structure length)
   (define (gen-beat-helper len recursion-depth)
     (let ([recurse (eq? (random 1 6) 2)]
           [subdivision (subdivide len 2)])
@@ -16,25 +17,45 @@
             (else
              (list len)))))
   (gen-beat-helper length 0))
-         
-(define (gen-random-notes length min max)
-  (let ([beat-structure (gen-beat-structure length)])
+
+;; Take a length in frames and generate notes
+;; between midi numbers min and max
+(define (gen-random-notes length letter-min octave-min letter-max octave-max)
+  (let ([beat-structure (gen-random-beat-structure length)]
+        [midi-min (letter-and-octave-to-midi letter-min octave-min)]
+        [midi-max (letter-and-octave-to-midi letter-max octave-max)])
         (map (lambda (x) (make-note-from-midi-num
-                          (random min max) x)) beat-structure)))
+                          (random midi-min midi-max) x)) beat-structure)))
 
-(define (gen-random-measure min max)
-  (make-measure (gen-random-notes (whole-note-length) min max)))
+;; Generate a measure object containing random
+;; notes and a random 
+(define (gen-random-measure letter-min octave-min letter-max octave-max)
+  (make-measure
+   (gen-random-notes (whole-note-length) letter-min octave-min letter-max octave-max)))
 
-(define part1
+
+;;;;;;;;;;;;;;; TEST PLAYGROUND ;;;;;;;;;;;;;;;
+
+;; Create a higher randomly generated
+;; atonal staff part that is 10
+;; measures long
+(define soprano
   (make-staff-part soft-synth
-   (build-list 10 (lambda (x) (gen-random-measure 60 72)))))
+   (build-list 15 (lambda (x)
+                    (gen-random-measure 'C 5 'C 6)))))
 
-(define part2
+;; Create a lower randomly generated
+;; atonal staff part that is 10
+;; measures long
+(define bass
   (make-staff-part soft-synth
-   (build-list 10 (lambda (x) (gen-random-measure 36 48)))))
+   (build-list 15 (lambda (x) (gen-random-measure 'G 3 'G 4)))))
 
-(define staff1
+;; Create an ensemble-staff consisting
+;  of the two parts
+(define atonal-duet
   (make-ensemble-staff
-   (list part1 part2)))
+   (list soprano bass)))
 
-(play (e-staff->rsound staff1))
+;; Build the ensemble-staff into an rsound and play it
+(play (e-staff->rsound atonal-duet))
--- rsound-music-theory.rkt
+++ rsound-music-theory.rkt
@@ -8,9 +8,7 @@
 ;;              Interface for Racket's rsound 
 ;;              library. 
 ;;
-;; ToDo:
-;;       1) Staff Parts should be broken down into measures
-;;       2) Easy instrument creation interface
+;; 
 
 #lang racket
 (require rsound)
@@ -20,25 +18,33 @@
 ;; Provide procedures
 (provide (all-defined-out))
 
-;;;;;;;  BASIC NON MUSICAL UTILITIES ;;;;;;;;;;
+;;;;;;;  NON MUSICAL UTILITIES ;;;;;;;;;;
 
+;; Attach a tag to an item
 (define (attach-tag tag item)
   (cons tag item))
 
+;; Get an object's tag
 (define (get-tag object)
   (if (pair? object)
       (car object)
       'unknown))
+
+;; Untag an object
 (define (get-item object)
   (if (pair? object)
       (cdr object)
       'error))
 
+;; Take a procedure and call it x times
 (define (do-x-times proc x)
   (cond ((< x 2) (proc))
         (else
          (do-x-times proc (- x 1))
          (proc))))
+
+
+;;;;;;;;; MUSICAL UTILITIES ;;;;;;;;;;;;
 
 ;; Take a note letter name and convert it
 ;; to its relative midi representation 0-11
@@ -61,6 +67,8 @@
         ((eq? letter 'B) 11)
         (else -1)))
 
+;; Take a relative midi-representation 0-11
+;; and convert it to its letter name
 (define (midi-number->letter number)
   (define num (remainder number 12))
   (cond ((eq? num 0) 'C)
@@ -81,6 +89,7 @@
         ((eq? num 11) 'B)
         (else -1)))
 
+;; Get the octave of a midi number
 (define (midi-number->octave num)
   (quotient num 12))
 
@@ -93,10 +102,7 @@
 ;; length of an RSound piano tone
 (define default-whole-note-length 132300)
 
-(define (set-global-whole-note-length frames)
-  (set! default-whole-note-length frames))
-
-;; Define the length  of a whole note in frames
+;; Return the length of a whole note in frames
 (define (whole-note-length)
   default-whole-note-length)
 
@@ -122,7 +128,6 @@
 ;; duration of a dotted half note
 (define (clip-to-dotted-half-note rsnd)
   (clip rsnd 0 (dotted-half-note-length)))
-
 
 ;; Return 0.25 the value of whole-note-length
 (define (quarter-note-length)
@@ -196,54 +201,72 @@
 (define (clip-to-thirtysecond-note rsnd)
   (clip rsnd 0 (thirtysecond-note-length)))
 
+;; Get a note letter and octave and convert
+;; it to a pitch in Hz
 (define (letter-and-octave-to-freq letter octave)
   (midi-note-num->pitch (letter-and-octave-to-midi letter octave)))
 
+;; Take a beat in frames and subdivide it into equal parts.
+;; Return a list of the new subdivisions
 (define (subdivide beat subdivision)
   (build-list subdivision (lambda (x) (round (/ beat subdivision)))))
 
+
+;;;;;;;;;;;; NOTE OBJECT ;;;;;;;;;;;;;;
+
+;; Create a note object from it's letter name (capital symbol),
+;; its octave, and its duration in frames
+(define (make-note letter octave duration)
+  (attach-tag 'Note (list letter octave duration)))
+
+;; Create a note from a midi number and a
+;; length in frames
 (define (make-note-from-midi-num num length)
   (make-note (midi-number->letter num)
              (midi-number->octave num)
              length))
 
-;;;;;;;;;;;; NOTE OBJECT ;;;;;;;;;;;;;;
-
-(define (make-note letter octave duration)
-  (attach-tag 'Note (list letter octave duration)))
-
+;; Check if object is a note
 (define (note? x)
   (eq? (get-tag x) 'Note))
 
+;; Get letter name of note object
 (define (note->letter note)
   (if (note? note)
       (car (get-item note))
       (raise-type-error ' note->letter "Note" note)))
 
+;; Get octave of note object
 (define (note->octave note)
   (if (note? note)
       (cadr (get-item note))
       (raise-type-error 'note->octave "Note" note )))
 
+;; Get duration of note object in frames
 (define (note->duration note)
   (if (note? note)
       (caddr (get-item note))
       (raise-type-error 'note->duration "Note" note )))
 
+;; Get midi number of note object
 (define (note->midi-number note)
   (if (note? note)
       (letter-and-octave-to-midi (note->letter note) (note->octave note))
       (raise-type-error 'note->midi-number "Note" note )))
 
+;; Get frequency of note object in Hz
 (define (note->freq note)
   (if (note? note)
       (letter-and-octave-to-freq (note->letter note) (note->octave note))
       (raise-type-error 'note->freq "Note" note)))
 
 
+;; Take a note object and a procedure which can turn
+;; the note into an rsound.  Call the procedure with
+;; the note object as an argument
 (define (note->rsound note note-to-rsound-proc)
   (cond ((and (note? note) (procedure? note-to-rsound-proc))
-           (note-to-rsound-proc note))
+           (rs-filter (note-to-rsound-proc note) reverb))
          (else
           (if (procedure? note-to-rsound-proc)
              (raise-type-error 'arg1 "Note" note)
@@ -254,12 +277,16 @@
 
 ;;;;;;;;;;; REST OBJECT ;;;;;;;;;;;;;;
 
+
+;; Create a rest (silence) object
 (define (make-rest duration)
   (attach-tag 'rest duration))
 
+;; Check if object is a rest object
 (define (rest? x)
   (eq? (get-tag x) 'rest))
 
+;; Get duration of the rest object in frames
 (define (rest->duration rest)
   (if (rest? rest)
       (get-item rest)
@@ -268,20 +295,25 @@
 
 ;;;;;;;;;;;; MEASURE OBJECT ;;;;;;;;;;;;
 
+;; Create a measure object from a list of notes
 (define (make-measure notelist)
   (attach-tag
    'Measure (filter
             (lambda (x) (or (note? x) (rest? x)))
             notelist)))
 
+;; Check if object is a measure
 (define (measure? x)
   (eq? (get-tag x) 'Measure))
 
+;; Return the measure's note list
 (define (measure->notelist measure)
   (if (measure? measure)
       (get-item measure)
       (raise-type-error 'measure->notelist "Measure" measure)))
 
+;; Take a measure and a procedure that converts notes to rsounds
+;; and apply the procedure to all notes in the measure
 (define (measure->rsound measure note-to-rsound-proc)
   (cond ((and (measure? measure) (procedure? note-to-rsound-proc))
            (rs-append* (map note-to-rsound-proc (measure->notelist measure))))
@@ -290,6 +322,7 @@
              (raise-type-error 'arg1 "Measure" measure)
              (raise-type-error 'arg2 "procedure" note-to-rsound-proc)))))
 
+;; Concatenate two measure objects
 (define (append-measure m1 m2)
   (cond ((and (measure? m1) (measure? m2))
          (make-measure (append (measure->notelist m1)
@@ -299,8 +332,7 @@
              (raise-type-error 'arg1 "Measure" m1)
              (raise-type-error 'arg2 "Measure" m1)))))
 
-                
-
+;; Concatenate a list of measure objects
 (define (append-measure* measurelist)
   (foldl (lambda (x y)
            (if (eq? y '())
@@ -310,6 +342,8 @@
 
 ;;;;;;;;;;;;; INSTRUMENT OBJECT ;;;;;;;;;;;;;;
 
+;; Create an instrument object which holds a name (symbol) and a
+;; procedure that converts notes to rsounds
 (define (make-instrument name note-to-rsound-proc)
   (cond ((and (procedure? note-to-rsound-proc) (symbol? name))
          (attach-tag 'Instrument (cons name note-to-rsound-proc)))
@@ -318,20 +352,25 @@
              (raise-type-error 'arg1 "symbol" name)
              ((raise-type-error 'arg2 "procedure" note-to-rsound-proc))))))
 
+;; Check if object is an instrument object
 (define (instrument? x)
   (eq? (get-tag x) 'Instrument))
 
+;; Get instrument name (symbol)
 (define (instrument->name instrument)
   (if (instrument? instrument)
       (car (get-item instrument))
       (raise-type-error 'instrument->name "Instrument" instrument)))
 
+;; Take an instrument's procedure and wrap it to ensure it
+;; can handle rest objects
 (define (instrument-proc-wrapper instrument-proc)
   (lambda (note)
     (if (rest? note)
         (silence (rest->duration note))
         (instrument-proc note))))
-        
+
+;; Return the instrument's internal conversion procedure
 (define (instrument->proc instrument)
   (if (instrument? instrument)
       (instrument-proc-wrapper
@@ -341,67 +380,79 @@
 
 ;;;;;;;;;;;;;; STAFF PART OBJECT ;;;;;;;;;;;;;;;;;
 
+;; Create a staff-part object from an instrument and a
+;; list fo measures
 (define (make-staff-part instrument measurelist)
   (cond ((instrument? instrument)
-         (attach-tag 'StaffPart (cons
-                                 instrument
-                                 (append-measure* (filter measure? measurelist)))))
+         (attach-tag 'StaffPart (cons instrument (filter measure? measurelist))))
         (else
          (raise-type-error 'make-staff-part "procedure" instrument))))
 
+;; Check if object is a staff-part object
 (define (staff-part? x)
   (eq? (get-tag x) 'StaffPart))
 
+;; Get the staff-part's instrument
 (define (staff-part->instrument staff-part)
   (if (staff-part? staff-part)
       (car (get-item staff-part))
       (raise-type-error 'staff-part->instrument "StaffPart" staff-part)))
 
-(define (staff-part->measure staff-part)
+;;  Get the staff-part's measures
+(define (staff-part->measurelist staff-part)
   (if (staff-part? staff-part)
       (cdr (get-item staff-part))
       (raise-type-error 'staff-part->instrument "StaffPart" staff-part)))
 
+;; Convert a staff-part into an rsound
 (define (staff-part->rsound staff-part)
  (if (staff-part? staff-part)
      (measure->rsound
-      (staff-part->measure staff-part)
+      (append-measure*
+       (staff-part->measurelist staff-part))
       (instrument->proc (staff-part->instrument staff-part)))
      (raise-type-error 'staff-part->instrument "StaffPart" staff-part)))
 
+;; Concatenate two staff-parts
 (define (append-staff-part part1 part2)
     (cond ((and (staff-part? part1) (staff-part? part2))
          (make-staff-part (staff-part->instrument part1)
-                          (append-measure (staff-part->measure part1)
-                                         (staff-part->measure part2))))
+                          (append (staff-part->measurelist part1)
+                                  (staff-part->measurelist part2))))
          (else
            (if (staff-part? part1)
              (raise-type-error 'arg1 "StaffPart" part2)
              (raise-type-error 'arg2 "StaffPart" part1)))))
 
-(define (append-staff-part* staff-partlist)
+;; Concatenate a list of staff-parts
+(define (append-staff-part* partlist)
   (foldl (lambda (x y)
            (if (eq? y '())
                x
-                (append-staff-part x y))) '() staff-partlist))
+                (append-staff-part x y))) '() partlist))
 
 ;;;;;;;;;;;;;; ENSEMBLE STAFF OBJECT ;;;;;;;;;;;;;;;;
 
+;; Create an ensemble part object
 (define (make-ensemble-staff partlist)
   (attach-tag
    'EnsembleStaff (filter (lambda (x) (staff-part? x)) partlist)))
 
+;; Check if object is an ensemble part object
 (define (e-staff? x)
   (eq? (get-tag x) 'EnsembleStaff))
 
+;; Get an ensemble-staff's list of staff parts
 (define (e-staff->partlist staff)
   (if (e-staff? staff)
       (get-item staff)
       (raise-type-error 'e-staff->instrument "EnsembleStaff" staff)))
 
+;; Convert an ensemble-staff object into an rsound
 (define (e-staff->rsound staff)
   (rs-overlay* (map staff-part->rsound (e-staff->partlist staff))))
 
+;; Concatenate two ensemble staff objects
 (define (append-ensemble-staff e-staff1 e-staff2)
     (cond ((and (e-staff? e-staff1) (e-staff? e-staff2))
          (make-ensemble-staff
@@ -413,25 +464,27 @@
              (raise-type-error 'arg1 "EnsembleStaff" e-staff2)
              (raise-type-error 'arg2 "EnsembleStaff" e-staff1)))))
 
+;; Concatenate a list of ensemble-staff objects
 (define (append-ensemble-staff* e-stafflist)
   (foldl (lambda (x y)
            (if (eq? y '())
                x
                 (append-ensemble-staff x y))) '() e-stafflist))
 
-
-
-
 ;;;;;;;;;; SYNTHESIZERS ;;;;;;;;;;;;
 
-
+;; Create a procedure that converts a note to
+;; an rsound with the waveform of rsound/single-cycle's
+;; vgame synth #82
 (define (soft-synth-rsound note)
   (cond ((rest? note)
          (silence (rest->duration note)))
         ((note? note)
-         (synth-note "vgame" 76 (note->midi-number note) (note->duration note)))))
+         (synth-note "vgame" 82 (note->midi-number note) (note->duration note)))))
 
 (define soft-synth (make-instrument 'SoftSynth soft-synth-rsound))
+
+
 ;;;;;;; OTHER NOTES ;;;;;;;;;
 
 ;; vgame sounds
